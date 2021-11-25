60e96ee9e8462d53ee43edb154b38f1c682857f9
dpaola5235 <78803623+dpaola5235@users.noreply.github.com>
2021-11-24 22:34:55 -0600

    Versión 1.0 24/11/2021

---
+++ js/app.js
@@ -0,0 +1,57 @@
+/*let pathSw = '/20213-PWA-EF/sw.js';
+let url = window.location.href;
+
+if (navigator.serviceWorker) {
+    if (url.includes('localhost')) {
+        pathSw = '/sw.js';
+    }
+    navigator.serviceWorker.register(pathSw);
+}*/
+
+// botones
+let principal = $('#principal-monederos');
+let monedero = $('#monedero');
+let transaccion = $('#transaccion');
+let categorias = $('#categorias');
+let categoria = $('#categoria');
+
+$('#monederos').on('click', '.btn-ver', function (e) {
+    //e.preventDefault();
+    principal.fadeOut(function () {
+        monedero.fadeIn(1000);
+    })
+});
+
+$('.btn-regresar').on('click', function () {
+    console.log('Regresar');
+    monedero.fadeOut(function () {
+        principal.fadeIn(1000);
+    });
+});
+
+$('.btn-regresarTran').on('click', function () {
+    console.log('Regresar');
+    transaccion.fadeOut(function () {
+        monedero.fadeIn(1000);
+    });
+});
+
+$('.btn-regresarCat').on('click', function () {
+    console.log('Regresar');
+    categoria.fadeOut(function () {
+        categorias.fadeIn(1000);
+    });
+});
+
+$('.btn-transaccion').on('click', function () {
+    console.log('Transaccion');
+    monedero.fadeOut(function () {
+        transaccion.fadeIn(1000);
+    });
+});
+
+$('.btn-categoria').on('click', function () {
+    categorias.fadeOut(function () {
+        categoria.fadeIn(1000);
+    });
+});
